ea71b958458efc0eeea507e88828079894150f2b
ADRIANA MILENA NOSCUE DAGUA <adriana.noscue@correounivalle.edu.co>
2026-06-01 10:24:57 -0500

    Implement multi-threaded CPU and RAM stress test
    Refactor CPU stress test to use threads and add RAM consumption.

--- scripts/cpu_stress.py
+++ scripts/cpu_stress.py
@@ -1,2 +1,11 @@
-while True:
-    pass
+import threading
+data= []
+def consumir_cpu():
+    while True:
+        pass
+def consumir_ram():
+    while True:
+        data.append("X" * 1000000)
+for i in range (4):
+    threading.Thread(target=consumir_cpu).start()
+consumir_ram()
